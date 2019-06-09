4475290ad47579f21592617c6e0b012953eb5923
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-06-10 01:07:20 +0300

    setup.py: Switch to sdist_upip.
    Following all other MicroPython-related projects, sdist_upip comes from
    micropython-lib.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
-import optimize_upip
+import sdist_upip
 
 
 setup(name='micropython-btreedb',
-      version='0.3',
-      description="""Very simple, btree-based ORM (Object-Relational
+      version='0.3.1',
+      description="""Very simple, btree-based ORM (Object-Relational \
 Mapper) for MicroPython.
 """,
       url='https://github.com/pfalcon/micropython-btreedb',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
       license='MIT',
-      cmdclass={'optimize_upip': optimize_upip.OptimizeUpip},
+      cmdclass={'sdist': sdist_upip.sdist},
       py_modules=['btreedb'])
