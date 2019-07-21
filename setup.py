27cf46132cd23ea84a8f7a12715359724bc2a9ad
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-07-21 09:22:00 +0300

    setup.py: Update for Pycopy project.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 import sdist_upip
 
 
-setup(name='micropython-btreedb',
-      version='0.3.2',
+setup(name='pycopy-btreedb',
+      version='0.4',
       description="""Very simple ORM (Object-Relational \
-Mapper) for MicroPython's 'btree' module.
+Mapper) for Pycopy's 'btree' module.
 """,
-      url='https://github.com/pfalcon/micropython-btreedb',
+      url='https://github.com/pfalcon/pycopy-btreedb',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
       license='MIT',
